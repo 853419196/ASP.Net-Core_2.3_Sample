eda1ea8b5a67a8ee542341d74f00bc0da79d71e1
砂漠の毒虫 <28293998+853419196@users.noreply.github.com>
2025-02-23 19:34:03 +0900

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("[action]/{id?}")]
+        public ActionResult<String> Index([FromRoute] String id)
+        {
+            return $"Hello, {id ?? "World"}!";
+        }
+    }
+}
